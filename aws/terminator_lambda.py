fff27a1141e93846a2f549c2167f18bcc891c6ba
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2024-12-09 15:12:41 +0100

    Update policies (#10)
    * add new policy allowing terminator to delete resources from multiple regions
    
    * move policies to dedicated file
    
    * Move smallset policy into aws/policy

--- aws/terminator_lambda.py
+++ aws/terminator_lambda.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 from terminator import (
     cleanup,
@@ -7,26 +6,19 @@ from terminator import (
 )
 
 
-def check_boolean_value(env_name: str) -> bool:
-    value = os.environ.get(env_name) or "false"
-    return value.lower() in ['true', '1', 'yes']
-
-
 # noinspection PyUnusedLocal
 def lambda_handler(event, context):
     # pylint: disable=unused-argument
     logger.setLevel(logging.INFO)
 
-    targets = []
-    if check_boolean_value("TERMINATE_SMALL_SET"):
-        targets = [
-            "RdsDbCluster",
-            "Ec2Volume",
-            "Ec2Snapshot",
-            "Ec2Instance",
-            "Ec2Eip",
-            "Ec2NatGateway",
-            "Ec2InternetGateway"
-        ]
+    targets = [
+        "RdsDbCluster",
+        "Ec2Volume",
+        "Ec2Snapshot",
+        "Ec2Instance",
+        "Ec2Eip",
+        "Ec2NatGateway",
+        "Ec2InternetGateway"
+    ]
 
     cleanup(check=False, force=False, targets=targets)
